1a2e4c316acab6b65c0fff3fba3c10d4ccec80e5
Damien Clarke <dxinteractive@gmail.com>
2020-02-06 22:48:23 +1100

    docs: fix docs build

--- packages/react-floatybox-docs/src/pages/index.js
+++ packages/react-floatybox-docs/src/pages/index.js
@@ -1,53 +1,13 @@
 // @flow
 import React from 'react';
-// $FlowFixMe
-import {useState} from 'react';
-// $FlowFixMe
-import {useCallback} from 'react';
 import Page from 'component/Page';
 import {H1} from 'dcme-style';
 import {Box} from 'dcme-style/layout';
 import {Text} from 'dcme-style/affordance';
 import {Link} from 'dcme-style/affordance';
-import {ContentNav} from 'dcme-style';
-import styled from 'styled-components';
-
-import FloatyBox from 'react-floatybox';
-import Point from 'react-floatybox/Point';
-
-import ColorPicker from '../component/ColorPicker';
-import IndexMdx from './indexMdx.mdx';
-
-const Tooltip = styled.div`
-    background-color: #000;
-    color: #FFF;
-    padding: 1rem;
-`;
-
-const Wow = styled.span`
-    font-weight: 700;
-    cursor: pointer;
-`;
+import IndexMdx from '../mdx/index.mdx';
 
 export default () => {
-    // // state for "Control the state yourself" example
-    // let [isOpen, setOpen] = useState(false);
-
-    // // state for color picker example
-    // let [color, setColor] = useState('#F00');
-
-    // let tooltip = useCallback(() => {
-    //     return <Tooltip>Hello I'm a tooltip.</Tooltip>;
-    // }, []);
-
-    // let tooltipWithTail = useCallback(({tailProps}) => {
-    //     return <Tooltip>Hello I'm a tooltip. <Point {...tailProps} color="#000" /></Tooltip>;
-    // }, []);
-
-    // let closableTooltip = useCallback(({close}) => {
-    //     return <Tooltip>Tooltip <Wow onClick={close}>[x]</Wow>.</Tooltip>;
-    // }, []);
-
     return <Page>
         <Box pt={[3,4]} px={[3,4]} pb="100rem" maxWidth="800px" margin="0 auto">
             <Box mb={4}>
